17cd34344a3b02b8dc713ce670bb42ed7b4ab3da
Kristen Hallstrom <klhalls89@gmail.com>
2019-05-30 12:55:52 -0600

    Add buttons

--- src/App.js
+++ src/App.js
@@ -2,47 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 
 const API_KEY = `${process.env.REACT_APP_API_KEY}`
-const url = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${API_KEY}`
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      book: []
-    };
   };
 
-  componentDidMount = () => {
-    this.fetchBooks()
+  selectBookType = (e) => {
+    let bookType = e.target.value
+    this.fetchBooks(bookType)
   }
-
-  fetchBooks = () => {
+ 
+  fetchBooks = (bookType) => {
     console.log('fetchBooks')
+    console.log(bookType)
+    const url = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-${bookType}.json?api-key=${API_KEY}`
     fetch(url)
     .then(response => response.json())
-    .then(data => this.resetState(data.results.books))
-  }
-
-  resetState = (books) => {
-    this.setState({
-      books
-    })
+    .then(data => console.log(data.results.books))
   }
 
   render() {
-    console.log(url)
-    console.log(this.state.books)
     return (
       <div className="App">
         <h1>Summer Book Club</h1>
+        <button onClick={(e) => this.selectBookType(e)} value='fiction'>fiction</button>
+        <button onClick={(e) => this.selectBookType(e)} value='nonfiction'>nonfiction</button>
       </div>
     );
   };
 };
-
-
-
- 
-
 
 export default App;
