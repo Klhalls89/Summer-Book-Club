d901f3b6f02b2b899105f448e10e0db67f05a595
Kristen Hallstrom <klhalls89@gmail.com>
2019-05-30 08:50:30 -0600

    Get Api key working

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 const API_KEY = `${process.env.REACT_APP_API_KEY}`
-const url = 'https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=' + API_KEY
+const url = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${API_KEY}`
 
 class App extends Component {
   constructor() {
@@ -12,25 +12,26 @@ class App extends Component {
     };
   };
 
-  // componentDidMount = () => {
-  //   this.fetchBooks()
-  // }
+  componentDidMount = () => {
+    this.fetchBooks()
+  }
 
-  // fetchBooks = () => {
-  //   console.log('fetchBooks')
-  //   fetch(url)
-  //   .then(response => response.json())
-  //   .then(data => this.resetState(data.results.books))
-  // }
+  fetchBooks = () => {
+    console.log('fetchBooks')
+    fetch(url)
+    .then(response => response.json())
+    .then(data => this.resetState(data.results.books))
+  }
 
-  // resetState = (books) => {
-  //   this.setState({
-  //     books
-  //   })
-  // }
+  resetState = (books) => {
+    this.setState({
+      books
+    })
+  }
 
   render() {
-    console.log(API_KEY)
+    console.log(url)
+    console.log(this.state.books)
     return (
       <div className="App">
         <h1>Summer Book Club</h1>
