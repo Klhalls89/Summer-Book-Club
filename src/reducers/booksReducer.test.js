8ec418c059ada8894af5f08744c134bcc6520b6e
Kristen Hallstrom <klhalls89@gmail.com>
2019-06-02 21:29:28 -0600

    Add store books case reducer test

--- src/reducers/booksReducer.test.js
+++ src/reducers/booksReducer.test.js
@@ -5,9 +5,20 @@ describe('booksReducer', () => {
 
   it('it should return inital', () => {
     const expected = []
-
     const result = booksReducer(undefined, {})
 
     expect(result).toEqual(expected)
   })
+  describe('Case STORE_BOOKS', () => {
+
+    const books = [{title: 'book1'}]
+
+    it('Should add books to the state tree', () => {
+      const action = actions.storeBooks(books)
+      const results = booksReducer([], action)
+      const expected = books
+
+      expect(results).toEqual(expected)
+    })
+  }) 
 })
