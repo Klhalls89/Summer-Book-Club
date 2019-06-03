c2112feb3c0b30352f5435bac3b024213ff9a457
Kristen Hallstrom <klhalls89@gmail.com>
2019-06-02 21:41:05 -0600

    Add errorRedcuer setError test

--- src/reducers/errorReducer.test.js
+++ src/reducers/errorReducer.test.js
@@ -9,5 +9,16 @@
 
     expect(results).toEqual(expected)
   })
- 
+  describe('Case SET_ERROR', () => {
+
+    const error = 'error'
+
+    it('should set an error to the state tree', () => {
+      const expected = error
+      const action = actions.setError(error) 
+      const results= errorReducer('', action)
+
+      expect(results).toEqual(expected)
+    })
+  }) 
 })
