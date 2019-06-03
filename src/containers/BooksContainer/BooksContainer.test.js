d14aafa49c55d1e317f5213375e262b45492c031
Kristen Hallstrom <klhalls89@gmail.com>
2019-06-02 20:18:18 -0600

    Add mstp test on booksContainer

--- src/containers/BooksContainer/BooksContainer.test.js
+++ src/containers/BooksContainer/BooksContainer.test.js
@@ -1,4 +1,22 @@
 import BooksContainer from './BooksContainer'
 import { mapStateToProps } from './BooksContainer'
 
-descr
+describe('BooksContainer', () => {
+
+  describe('mapStateToProps', () => {
+
+    it('should return a props object from state', () => {
+      const mockState = {
+        loading: false,
+        books: [],
+        error: 'error'
+      }
+      const expected = {
+        books: []
+      }
+      const results = mapStateToProps(mockState)
+      expect(results).toEqual(expected)
+
+    })
+  })
+})
