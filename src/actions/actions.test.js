e43f5fdf0ad3a33151c97113c1d4a4775ee6d7b9
Kristen Hallstrom <klhalls89@gmail.com>
2019-06-02 20:48:34 -0600

    Add storeBooks action test

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -2,16 +2,31 @@ import * as actions from './actions'
 
 describe('actions', () => {
 
+  describe('storeBooks', () => {
+
+    it('should have a type of STORE_BOOKS', () => {
+      const books = [{titel: 'This is us'}]
+      const expectedAction = {
+        type: 'STORE_BOOKS',
+        books
+      }
+      const results = actions.storeBooks(books)
+
+      expect(results).toEqual(expectedAction)
+    })
+  })
+
   describe('toggleLoading', () => {
+
     it('should have a type of TOGGLE_LOADING', () => {
       const bool = true
       const expectedAction = {
         type: 'TOGGLE_LOADING',
         bool
       }
-      const result = actions.toggleLoading(bool)
+      const results = actions.toggleLoading(bool)
 
-      expect(result).toEqual(expectedAction)
+      expect(results).toEqual(expectedAction)
     })
   })
 })
