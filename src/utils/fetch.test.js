584f71a810dba5db2f702d0750b982b04b8145e9
Kristen Hallstrom <klhalls89@gmail.com>
2019-06-04 21:47:05 -0600

    Try fetch test again

--- src/utils/fetch.test.js
+++ src/utils/fetch.test.js
@@ -2,17 +2,31 @@ import React from 'react';
 import { booksCleaner } from './cleaner';
 import { fetchBooks } from './fetch';
 
+jest.mock('./cleaner')
+
 describe ('fetchBooks', () => {
   const mockBooks = [{title: 'the best one'},{title: 'these cats'}];
 
   window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
-    json: () => Promise.resolve(mockBooks)
+    json: () => Promise.resolve(mockBooks),
+    ok: true
   }));
 
-  it('calls fetch with the correct data', () => {
+  it.skip('calls fetch with the correct data', () => {
+    window.fetch(url)
     const url = 'www.url.com'
     expect(window.fetch).toHaveBeenCalledWith(url)
   }); 
+
+  it.skip('calls booksCleaner after fetch', async() => {
+    booksCleaner = jest.fn()
+    await window.fetch()
+    expect(booksCleaner).toHaveBeenCalled()
+  })
+
+  //should error if fetch failed
+
+
 });
 
 
